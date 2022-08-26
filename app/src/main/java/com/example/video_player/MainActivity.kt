7baf3e1dad10ffe5f59a347eee6733412514c7f3
Gemma-Usiku <afrogem3@gmail.com>
2022-08-26 12:24:58 +0200

    added VideoView class to main activity

--- app/src/main/java/com/example/video_player/MainActivity.kt
+++ app/src/main/java/com/example/video_player/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.video_player
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.VideoView
+import android.net.Uri
+import android.net.Uri.*
+import android.view.View
+import android.widget.MediaController
+
 
 class MainActivity : AppCompatActivity() {
+    val videoView:  VideoView? = null
+
+    //val mediaController: MediaController? =  null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val videoView = findViewById<View>(R.id.testView) as VideoView
+        val mediaController = MediaController(this)
+        mediaController.setAnchorView(videoView)
+        //specify the location of media file
+        val uri: Uri = parse(
+            "android.resource://" + packageName
+                    + "/raw/test"
+        )
+        //Setting MediaController and URI, then starting the videoView
+        videoView.setMediaController(mediaController)
+        videoView.setVideoURI(uri)
+        videoView.requestFocus()
+        videoView.start()
+
     }
 }
+
+
+
+
